04ccba69a40960cfbc75240bd49492a8e6c83be3
Pallavi4546 <82462352+Pallavi4546@users.noreply.github.com>
2023-02-13 14:49:02 +0530

    constructor function

--- arrays.js
+++ arrays.js
@@ -66,3 +66,86 @@ class SortedArray{
 }
 const result1 = new SortedArray([0,1,0,3,2,5,3,2,5,1,0])
 // console.log("sorted Array",result1.sortedArray())
+
+// Move all negative numbers to beginning and positive to end with constant extra space
+class SeperatePositiveandNegative{
+    constructor(arr){
+        this.arr=arr
+        this.res = function seperation(){
+            const positive = this.arr.filter((val)=>{if(val > 0){
+                return val
+            }
+            })
+            const negative = this.arr.filter((val)=>{if(val < 0){
+                return val
+            }
+            })
+            return [...negative,...positive]
+        }
+    }
+}
+const result2 = new SeperatePositiveandNegative([-10,20,3,-2,-16,-15,8,2,9,-4])
+// console.log("result2",result2.res())
+
+// Union of two arrays
+class Union{
+    constructor(arr1,arr2){
+        this.arr1 = arr1
+        this.arr2 = arr2
+        this.resUnion = function union(){
+            return [... new Set([...this.arr1,...this.arr2])]
+        }
+        this.resIntersection = function intersection(){
+            const newArray =  this.arr1.filter((val)=>{
+                if(this.arr2.includes(val)){
+                    return val
+                }
+            })
+            return newArray
+        }
+    }
+}
+const result3 = new Union([1, 2 ,3, 4, 5],[1,2,3])
+// console.log("result3",result3.resIntersection())
+// console.log("result3",result3.resUnion())
+
+// Given an array, rotate the array by one position in clock-wise direction.
+class RotateArrayByOne{
+    constructor(arr,position){
+        this.arr = arr
+        this.position = position
+        this.resRotateByPosition = function rotateByOne(){
+const temp = this.arr[this.arr.length-1]
+        this.arr.splice(this.arr.length-1,1)
+        this.arr.unshift(temp)
+return this.arr
+       }
+    }
+}
+const result4 = new RotateArrayByOne([1, 2 ,3, 4, 5,8,9])
+// console.log("result4",result4.resRotateByPosition())
+
+// Kadane's Algorithm
+class Solution1{
+    //Function to find the sum of contiguous subarray with maximum sum.
+    constructor(arr){
+        this.arr=arr
+      this.sum=0;
+      this.max=this.arr[0];
+      this.res= function result(){
+        for(let i= 0; i<this.arr.length; i++) {
+            this.sum += this.arr[i];
+            if(this.sum > this.max) {
+              this.max = this.sum;
+            }
+            if(this.sum < 0){
+              this.sum=0;
+            }
+        }
+        return this.max;
+      }
+    
+    }
+}
+const result5 = new Solution1([-11,2,3,-2,5])
+console.log("result5",result5.res())
